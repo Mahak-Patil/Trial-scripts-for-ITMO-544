8ee4910394c46ddce4c9c2d554535b492e5e3705
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:12:24 -0600

    more code

--- creating-subnets.sh
+++ creating-subnets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+VpcID=$(aws ec2 create-vpc --cidr-block 172.3.0.0/28 --output=text |awk 'ami-d05e75b8')
+echo $VpcID
 SubnetID=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 172.3.0.0/28 --output=text|awk 'ami-d05e75b8')
 echo $SubnetID
