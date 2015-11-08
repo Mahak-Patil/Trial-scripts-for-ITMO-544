bdcec5cd0841aa1e551f763dd0d39d98601201dd
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:19:40 -0600

    final script to create vpc and corresponding subnets

--- creating-subnets.sh
+++ creating-subnets.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# PLAY MORE WITH CIDR BLOCKS!!!
+# needs only image id to function. Output in text form. 
+
 echo "Enter image id: (ami-d05e75b8)"
 
 VpcID=$(aws ec2 create-vpc --cidr-block 172.3.0.0/28 --output=text |awk '{print $1}')
 echo "\n"$VpcID
-SubnetID=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 172.3.0.0/28 --output=text|awk '{print $1}')
+SubnetID=$(aws ec2 create-subnet --vpc-id $VpcID --cidr-block 172.3.0.0/28 --output=text|awk '{print $1}')
 echo "\n"$SubnetID
 
 echo "Sleeping for one minute"
@@ -12,3 +16,4 @@ do
 echo -ne '.'
 sleep 1
 done
+echo "Done sleeping!"
