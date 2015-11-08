7faebc56f96f42a3a68693f190e4fe81f0fec03a
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:28:24 -0600

    trying to remove hard coded subnet values, removing sleep option

--- creating-db-subnet.sh
+++ creating-db-subnet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # subnets hard coded: subnet-0fdfdd78 and subnet-f7a25eca
+# remove hardcoded subnet values.
+# use creating-subnets to get subnet ids and pass as arguments
 echo "creating db subnet with 2 subnets (needed)"
 DbSubnetID=$(aws rds create-db-subnet-group --db-subnet-group-name ITMO544-Database-Subnet --subnet-ids subnet-0fdfdd78 subnet-f7a25eca --db-subnet-group-description "Database subnet" --output=text)
 echo $DbSubnetID
