abcb0b0c2416f85170b60f524cf2b769c12a2fd0
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:49:29 -0600

    more code

--- creating-security-groups.sh
+++ creating-security-groups.sh
@@ -2,5 +2,6 @@
 # creating security group usong vpc "vpc-2ea45e4a"
 #while transfering, DO NOT pass hard coded vpc
 
+aws ec2 delete-security-group --group-name ITMO-544-Security-Group
 SecurityGroupID=$(aws ec2 create-security-group --group-name ITMO-544-Security-Group --description "Security group" --vpc-id vpc-2ea45e4a --output=textawk|'{print $1}')
 echo "\n"$SecurityGroupID
