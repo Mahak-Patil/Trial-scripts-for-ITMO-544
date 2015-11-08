533ea678023f5a9099762e8043c676f80c36902e
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:26:29 -0600

    hardcoded subnet ids and created db subnet

---
+++ creating-db-subnet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# subnets hard coded: subnet-0fdfdd78 and subnet-f7a25eca
+echo "creating db subnet with 2 subnets (needed)"
+DbSubnetID=$(aws rds create-db-subnet-group --db-subnet-group-name ITMO544-Database-Subnet --subnet-ids subnet-0fdfdd78 subnet-f7a25eca --db-subnet-group-description "Database subnet" --output=text)
+echo $DbSubnetID
+
+echo "Sleeping for one minute"
+for i in {0..60}
+do
+echo -ne '.'
+sleep 1
+done
+echo "Done sleeping!"
