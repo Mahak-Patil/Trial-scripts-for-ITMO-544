9f35a040daed807c42a494f53827d06b9bf37d88
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 14:16:41 -0600

    implemeting sleep schedule

--- creating-subnets.sh
+++ creating-subnets.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+echo "Enter image id: (ami-d05e75b8)"
 
-VpcID=$(aws ec2 create-vpc --cidr-block 172.3.0.0/28 --output=text |awk 'ami-d05e75b8')
-echo $VpcID
-SubnetID=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 172.3.0.0/28 --output=text|awk 'ami-d05e75b8')
-echo $SubnetID
+VpcID=$(aws ec2 create-vpc --cidr-block 172.3.0.0/28 --output=text |awk '{print $1}')
+echo "\n"$VpcID
+SubnetID=$(aws ec2 create-subnet --vpc-id $vpcId --cidr-block 172.3.0.0/28 --output=text|awk '{print $1}')
+echo "\n"$SubnetID
+
+echo "Sleeping for one minute"
+for i in {0..60}
+do
+echo -ne '.'
+sleep 1
+done
